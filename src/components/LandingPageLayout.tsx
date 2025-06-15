8c045dcffb367ac0f4032584fe04d8aab092263a
Chris Ftoulis <christosftoulis3@gmail.com>
2025-06-15 13:56:14 +0300

    feat: change background for login and register page

--- src/components/LandingPageLayout.tsx
+++ src/components/LandingPageLayout.tsx
@@ -1,13 +1,19 @@
 import LandingPageHeader from "./LandingPageHeader.tsx";
-import {Outlet} from "react-router";
+import {Outlet, useLocation} from "react-router";
 
 const LandingPageLayout = ({ children }: {children: React.ReactNode}) => {
 
+    const location = useLocation();
+    const isAuthPage = location.pathname.includes("/landing/login") || location.pathname.includes("/landing/register");
+
+    const containerClasses = isAuthPage
+        ?"container mx-auto pt-24 min-h-screen bg-[url('/homepagebg.png')] bg-cover bg-center pb-24\""
+        :"container mx-auto pt-24 min-h-screen bg-[url('/sporttickbg.png')] bg-cover bg-center pb-24\""
 
     return (
         <>
             <LandingPageHeader />
-            <div className="container mx-auto pt-24 min-h-screen bg-[url('/sporttickbg.png')] bg-cover bg-center pb-24">
+            <div className={containerClasses}>
                 {children}
                 <Outlet />
             </div>
